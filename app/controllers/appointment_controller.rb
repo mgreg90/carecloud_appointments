1c1133ccaf4226fb17794f96160f4449f6a5a65d
Michael Gregory <mgregory8219@gmail.com>
2016-09-02 16:11:29 -0400

    added test outlines and added empty controller methods

--- app/controllers/appointment_controller.rb
+++ app/controllers/appointment_controller.rb
@@ -1,3 +1,33 @@
 class AppointmentController < ApplicationController
-  
+
+  def show
+
+  end
+
+  def index
+
+  end
+
+  def create
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+private # -------------------------- private -----------------------------------
+
+  def search_params
+
+  end
+
+  def update_params
+
+  end
+
 end
---
+++ spec/models/appointment/post_spec.rb
@@ -0,0 +1,7 @@
+describe "Appointment API POST Request" do
+  it "creates a new appointment"
+  it "doesn't create a new appointment if there is no first_name"
+  it "doesn't create a new appointment if there is no last_name"
+  it "doesn't create a new appointment if there is no start_name"
+  it "doesn't create a new appointment if there is no end_name"
+end
