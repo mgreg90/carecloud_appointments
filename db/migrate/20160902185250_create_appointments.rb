75f04abc7f50f6d3007fa79a012a9195faa998d0
Michael Gregory <mgregory8219@gmail.com>
2016-09-02 15:06:48 -0400

    updated appointment model

--- db/migrate/20160902185250_create_appointments.rb
+++ db/migrate/20160902185250_create_appointments.rb
@@ -1,6 +1,11 @@
 class CreateAppointments < ActiveRecord::Migration[5.0]
   def change
     create_table :appointments do |t|
+      t.datetime :start_time
+      t.datetime :end_time
+      t.string :first_name
+      t.string :last_name
+      t.text :comments
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -16,6 +16,11 @@ ActiveRecord::Schema.define(version: 20160902185250) do
   enable_extension "plpgsql"
 
   create_table "appointments", force: :cascade do |t|
+    t.datetime "start_time"
+    t.datetime "end_time"
+    t.string   "first_name"
+    t.string   "last_name"
+    t.text     "comments"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
