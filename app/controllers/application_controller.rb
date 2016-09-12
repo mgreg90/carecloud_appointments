fd0433e0efe13ef6589c076c57de111e36bad597
Michael Gregory <mgregory8219@gmail.com>
2016-09-12 18:42:04 -0400

    added welcome screen to point users to README.md

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,6 @@
-class ApplicationController < ActionController::API
+class ApplicationController < ActionController::Base
+
+  def root
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root 'application#root'
+
   get '/appointments' => 'appointment#index', as: 'index'
   get '/appointments/:id' => 'appointment#show', as: 'show'
 
